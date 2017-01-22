9fc2d54c73581037ae26df26ae7f22cbaf021972
DeveshChau <deveshachaudhari@gmail.com>
2017-01-22 12:53:12 +0530

    Degital clock

--- js/digitalclock.js
+++ js/digitalclock.js
@@ -1,5 +1,3 @@
-<head>
-<script>
 function startTime() {
     var today = new Date();
     var h = today.getHours();
@@ -15,12 +13,3 @@ function checkTime(i) {
     if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
     return i;
 }
-</script>
-</head>
-
-<body onload="startTime()">
-
-<div id="txt"></div>
-
-</body>
-</html>
